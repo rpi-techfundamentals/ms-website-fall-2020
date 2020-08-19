6cf505a637e39adbb7ac6148a1c5a3888abc8ae6
Jason Kuruzovich <jkuruzovich@gmail.com>
2020-08-19 12:00:57 -0400

    push to public

--- push_shared.sh
+++ push_shared.sh
@@ -1,6 +1,6 @@
 cp /Users/jasonkuruzovich/Box/Documents/Into-ml-app/book_fall_2020.xlsx   ./book.xlsx
 source build.sh
-ghp-import -n -p -f introml/_build/html
+ghp-import -n -p -f quant/_build/html
 git add --all
 if [ -z "$1"]
 then
