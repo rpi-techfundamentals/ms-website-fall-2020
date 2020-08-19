f3f19d55969d32c9a6dabe297511db413ada58ae
Jason Kuruzovich <jkuruzovich@gmail.com>
2020-08-19 18:04:15 -0400

    push to public

--- scripts/builder.py
+++ scripts/builder.py
@@ -1,6 +1,7 @@
 import yaml
 import pandas as pd
 import subprocess
+import numpy as np
 from tabulate import tabulate
 DATE='Date'
 
@@ -34,13 +35,21 @@ def update_yaml_file(file, data):
 
 def pandas_to_md(df, file, title):
     if DATE in df.columns:
-        if pd.core.dtypes.common.is_datetime_or_timedelta_dtype(df[DATE]):
-            print("Converting datetime to ")
-            df[DATE]=df[DATE].dt.strftime('%m/%d')
+        #if pd.core.dtypes.common.is_datetime_or_timedelta_dtype(df[DATE]):
+        print("Converting datetime to ")
+        df[DATE]=df[DATE].dt.strftime('%m/%d')
+    df=df.fillna(-99)
+    float_cols = df.select_dtypes(include=[np.float]).columns
+    df[float_cols]=df[float_cols].astype(int)
+    df=df.astype(str)
+    df=df.replace('-99', ' ')
+
     s = title
     separator = "\n============================\n\n"
-    #table=df.to_markdown(tablefmt="grid")
-    table=tabulate(df, tablefmt="pipe", headers="keys")
+    pd.set_option('precision', 0)
+    table=df.to_markdown(tablefmt="pipe", headers="keys", showindex="never")
+
+    #table=tabulate(df, tablefmt="pipe", headers="keys")
     output= s+separator+table
     print("Outputting file:", file)
     with open(file, "w") as text_file:
