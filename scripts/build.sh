ca66a228a81042f24ab35786eda552796b2ac523
Jason Kuruzovich <jkuruzovich@gmail.com>
2020-08-21 23:16:38 -0400

    push to public

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-book=site/
+class=site/
 #get most recent notebook.
-jupyter nbconvert --to script convert.ipynb
-python convert.py
-jupyter-book build ../$book
+#jupyter nbconvert --to script convert.ipynb
+#python convert.py
+jupyter-book build ../$class
